17f6dfe509171c7aa38caa9c337e703bb87fbe3c
edupoly <info.edupoly@gmail.com>
2025-06-30 07:49:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const leadRoutes = require("./routes/leadRoutes");
+
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Student Lead API is running");
+});
+dotenv.config();
+connectDB();
+
+app.use("/api/leads", leadRoutes);
+
+const PORT = process.env.PORT || 4500;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
