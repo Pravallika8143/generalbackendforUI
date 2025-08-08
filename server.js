40e87e590c3523774878f26a3193f8319844c6b1
edupoly <info.edupoly@gmail.com>
2025-08-08 08:57:57 +0530

    todomodel

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const leadRoutes = require("./routes/leadRoutes");
+const todoRoutes = require("./routes/todoRoutes");
 const cors = require("cors");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,6 +15,7 @@ dotenv.config();
 connectDB();
 
 app.use("/api/leads", leadRoutes);
+app.use("/api/todos", todoRoutes);
 
 const PORT = process.env.PORT || 4500;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
